16242d5d4d11e07dfe045cf0c764ab24d44754e9
amal2004 <amal2004@gmail.com>
2018-09-13 21:30:34 -0400

    Initial commit before adding Lombok

---
+++ src/main/java/com/spring5/recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.spring5.recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
---
+++ src/main/java/com/spring5/recipeapp/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.spring5.recipeapp.repositories;
+
+
+import com.spring5.recipeapp.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+}
